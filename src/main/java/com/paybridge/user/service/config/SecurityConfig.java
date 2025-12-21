f3bdfd7716fb2f28d46a0ef8254c259ef583604f
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-12-21 16:14:55 +0700

    Add actuator and implement it

--- src/main/java/com/paybridge/user/service/config/SecurityConfig.java
+++ src/main/java/com/paybridge/user/service/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs*/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/health").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/api/v1/users/**").authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
